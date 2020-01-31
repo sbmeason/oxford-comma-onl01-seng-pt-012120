a6487311d9db7f5bda82a345dd701d55e35b7761
Samantha Meason <sbmeason@gmail.com>
2020-01-31 01:47:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,8 @@
 def oxford_comma(array)
-  if words.length == 2
-    return "#{words[0]} and #{words[1]}"
-  elsif words.length > 2
-    words[-1].insert(0 , " and ")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert(0 , "and ")
   end
   array.join(", ")    
 end
-end
-
-
